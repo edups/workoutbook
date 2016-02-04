dfdd93b74841d20f26400df41b73cc0408a4f532
edups <eduardo.pecinos_263@ausiasmarch.net>
2016-02-04 21:19:14 +0100

    Added list of exercises, sideNav and others

--- client/workouts/workouts.js
+++ client/workouts/workouts.js
@@ -2,3 +2,11 @@ Meteor.subscribe('workouts');
 
 //preparado para añadir Google Analytics
 //console.log(Meteor.settings.public.ga.account);
+
+//Añade un helper
+Template.workouts.helpers({
+	//coffescript function
+	workouts: ()=>{
+		return Workouts.find({});
+	}
+});
