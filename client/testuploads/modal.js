25174789f42e18a26c49cb6bc437fa13e1f73a24
edups <eduardo.pecinos_263@ausiasmarch.net>
2016-02-23 23:40:18 +0100

    Added more styles and test files

---
+++ client/testuploads/modal.js
@@ -0,0 +1,13 @@
+Template.modale.events({  
+  'click button.modal': function(event, cosa) {
+    var name = cosa.$(event.target).data('modalTemplate');
+    console.log(name);
+    Session.set('activeModal', name);
+  }
+});
+
+Template.modal.helpers({  
+  activeModal: function() {
+    return Session.get('activeModal');
+  }
+});
